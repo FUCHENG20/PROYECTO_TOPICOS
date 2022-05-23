11efb63a8d476e90a0ce6273a4c8d16538735686
FUCHENG20 <L20760444@ensenada.tecnm.mx>
2022-05-22 18:07:27 -0700

    Cambios en el Encabezado

--- citas/src/components/Cabecera.jsx
+++ citas/src/components/Cabecera.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Cabecera = () => {
     return (
         <div>
-            <h1 className=' sm:text-3xl text-xl font-bold bg-slate-700 text-center p-5 m-10 rounded-lg'>ADMINISTRADOR DE CITAS</h1>
+            <h1 className=' sm:text-3xl text-white text-xl font-bold bg-slate-700 text-center p-5 m-10 rounded-lg'>ADMINISTRADOR DE CITAS</h1>
         </div>
     )
 }
